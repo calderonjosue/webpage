1d35fa6914317ba6897c1e30e06be7f6bc2cc9e8
pipodco-byte <pipod.co@gmail.com>
2025-12-05 18:29:37 -0500

    fix: update GitHub profile URL

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -25,7 +25,7 @@ const social: Social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://github.com/josuecalderon",
+    link: "https://github.com/calderonjosue",
     essential: true,
   },
   {
